39f60f2624ca5ac9f752d655a407325a993e78e8
Rishabh Kushwaha <51046700+rishabhKush@users.noreply.github.com>
2024-12-23 21:09:52 +0400

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Configuration parameters
+
+# Path of whisper model
+WHISPER_MODEL_PATH = "openai/whisper-tiny"
+
+# Threshold values for silero VAD, may need to adjust
+VAD_THRESHOLD = 0.5
+VAD_WINDOW_SIZE = 512
+
+# How many top sentences to keep for TF-IDF summary
+SUMMARY_TOP_SENTENCES = 3
